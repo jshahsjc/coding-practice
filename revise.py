3fb80493900cb8236251c1f5eaa2489fd3be60cc
“Jaimin <jshah.sjc@gmail.com>
2020-03-14 14:32:21 -0700

    added new method for twoSum using Enumeration

--- revise.py
+++ revise.py
@@ -1,25 +1,9 @@
-def merge(L1, L2, A = []):
-   i = j = 0
-   while i < len(L1) and j < len(L2):
-      if L1[i] <= L2[j]:
-         A.append(L1[i])
-         i += 1
+def findTwoSum(given_nums, target):
+   res = {}
+
+   for i, num in enumerate(given_nums):
+      comp = target - num
+      if comp not in res:
+         res[num] = i
       else:
-         A.append(L2[j])
-         j += 1
-
-   while i < len(L1):
-      A.append(L1[i])
-      i += 1
-
-   while j < len(L2):
-      A.append(L2[j])
-      j += 1
-
-   return A
-
-l1 = [10, 12, 15, 21, 29]
-l2 = [13, 16, 22, 23, 24, 25]
-
-mergedList = merge(l1, l2)
-print mergedList
+         return [res[comp], i]
--- twoSum.py
+++ twoSum.py
@@ -28,9 +28,7 @@ increment index 2, iterate 3 (2+3)
 for each pair, compare with target, if hit, break.
 
 So, given 4 numbers, there are 6 possible pairs -- O[n(n-1)/2]
-
 """
-
 def findTwoSum(given_nums, target):
    for i in range(len(given_nums)):
       for j in range (i+1, len(given_nums)):
@@ -41,11 +39,30 @@ def findTwoSum(given_nums, target):
    else:
       return ("No match found")
 
+"""
+A better solution:
+   Use enumerate on a list. It allows to process an iterable object such as an array with a counter that starts at 0 by default along with each iteratated value.
+
+   1. Store the index(count) and value as we iterate through the array in an empty dictionary (hash table in general).
+   2. For each new value, check if the complement of that value (target - value) is in the dictionary.
+   3. If found, return the index of value and the complement value for the given target.
+
+   This solution is O(n) since it's using res{} and dictionary/hash table can lookup values in O(1). So, for the worst case, we may have to iterate through the whole list, which is O(N).
+"""
+def findTwoSumEnum(given_nums, target):
+   res = {}
+
+   for i, num in enumerate(given_nums):
+      comp = target - num
+      if comp not in res:
+         res[num] = i
+      else:
+         return [res[comp], i]
 
 my_num = [2, 7, 11, 15, 100, 350, 0, -40, -4]
-t = 101
+t = 100
 
-result_pair = findTwoSum(my_num, t)
+result_pair = findTwoSumEnum(my_num, t)
 print "given nums: " + str(my_num)
 print "target: " + str(t)
 print "result pair: " + str(result_pair)
