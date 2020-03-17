f5c604ab77423096e302fbd087bf1d0ede0f26e5
“Jaimin <jshah.sjc@gmail.com>
2020-03-16 21:56:17 -0700

    added height method

--- Tree.py
+++ Tree.py
@@ -35,14 +35,30 @@ class Tree:
             return self.right.find(data)
 
    def getsize(self):
-      if self.left is not None and self.right is not None:
-         return 1 + self.left.getsize() + self.right.getsize()
-      elif self.left:
-         return 1 + self.left.getsize()
-      elif self.right:
-         return 1 + self.right.getsize()
+      if self is not None:
+         if self.left is not None and self.right is not None:
+            return 1 + self.left.getsize() + self.right.getsize()
+         elif self.left:
+            return 1 + self.left.getsize()
+         elif self.right:
+            return 1 + self.right.getsize()
+         else:
+            return 1
       else:
-         return 1
+         return 0
+
+   def getheight(self):
+      if self is not None:
+         if self.left is not None and self.right is not None:
+            return 1 + max(self.left.getheight(), self.right.getheight())
+         elif self.left:
+            return  1 + self.left.getheight()
+         elif self.right:
+            return 1 + self.right.getheight()
+         else:
+            return 1
+      else:
+         return 0
 
    def preorder(self):
       if self is not None:
@@ -72,6 +88,8 @@ for j in range(50, 1000):
 print "From 50 to 1000, these data found in the tree" + str(res)
 
 print "size of the tree is: " +str(t.getsize())
+
+print "Height of the tree is: " + str(t.getheight())
 
 print "Preorder Tree:"
 t.preorder()
