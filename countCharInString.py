ce1f137be869b33a38383c23e6bd715f28621cc6
Jaimin Shah <62130656+jshahsjc@users.noreply.github.com>
2021-06-04 15:31:31 -0700

    count characters in string.

--- countCharInString.py
+++ countCharInString.py
@@ -19,6 +19,17 @@ def maxCharOccurance(S):
          maxChar, maxCount = k, v
    return maxChar, maxCount
 
+def repeatedChars(S):
+    resDict = charOccurance(S)
+    repeatChars = ''
+    for k, v in resDict.items():
+        if v > 1:
+            repeatChars += k
+    if len(repeatChars) > 0:
+        return repeatChars
+    else:
+        return 'No repeated characters found.'
+
 myStr = "anthony gonzalvis"
 resDict = charOccurance(myStr)
 print "All characters occurance: \n"+str(resDict)
