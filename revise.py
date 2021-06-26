e378b546c65583ce5dbc1f0522efdb6e067c05f7
Jaimin Shah <62130656+jshahsjc@users.noreply.github.com>
2021-06-26 09:52:18 -0700

    revise interval intersections

--- revise.py
+++ revise.py
@@ -1,20 +1,79 @@
 """
-check if a string is a pallindrome after removing a character
+LC 986. Interval List Intersections
+Medium
+
+You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+A closed interval [a, b] (with a < b) denotes the set of real numbers x with a <= x <= b.
+
+The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+Example 1:
+
+Input:
+firstList = [[0,2],[5,10],[13,23],[24,25]],
+secondList = [[1,5],[8,12],[15,24],[25,26]]
+Output:
+[[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+Example 2:
+
+Input: firstList = [[1,3],[5,9]], secondList = []
+Output: []
+
+Example 3:
+
+Input: firstList = [], secondList = [[4,8],[10,12]]
+Output: []
+
+Example 4:
+
+Input: firstList = [[1,7]], secondList = [[3,10]]
+Output: [[3,7]]
+
+Constraints:
+
+    0 <= firstList.length, secondList.length <= 1000
+    firstList.length + secondList.length >= 1
+    0 <= starti < endi <= 10^9
+    endi < starti+1
+    0 <= startj < endj <= 10^9
+    endj < startj+1
+
 """
 
-def check_pallindrome(S):
-    i = 0
-    j = len(S) - 1
-    is_pallindrome = True
-    while i <= j:
-        if S[i] == S[j]:
+def intervalIntersect(fl, sl):
+    # Need to take 2 pointers; one for each list of tuples
+    i = j = 0
+    res = []
+    # Approach is to compare a tuple from each list;
+    # If maximum of first element in each tuple = A;
+    # is less than minimum of second element = B;
+    # Then, there  is a common interval (A,B)
+    while i < len(fl) and j < len(sl):
+        leftEdge = max(fl[i][0],  sl[j][0])
+        rightEdge = min(fl[i][1], sl[j][1])
+        if leftEdge <=  rightEdge:
+            res.append((leftEdge, rightEdge))
+        # Thinking
+        # Need to decide how to move pointer
+        # If fl tuple right edge is less than
+        # sl tuple right edge;
+        # more comparisons are possible;
+        # So, move the fl pointer i;
+        # Otherwise move the sl pointer j.
+        if fl[i][1] <= sl[j][1]:
             i += 1
-            j -= 1
         else:
-            is_pallindrome = False
-            break
-    return is_pallindrome
+            j += 1
+    return res
 
-def checkIfPallindrome_after_removing_a_character(S):
-    for k in range(len(S) - 1):
-        tmpStr = S[:k] + S[k + 1:]
+firstList = [[0,2],[5,10],[13,23],[24,25]]
+secondList = [[1,5],[8,12],[15,24],[25,26]]
+desiredOp = [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+testOp = intervalIntersect(firstList, secondList)
+print testOp
+if desiredOp == testOp:
+    print "Test pass!"
