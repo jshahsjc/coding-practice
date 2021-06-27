4446b06b7a460a7310f58c8e0a357a67c17b4602
Jaimin Shah <62130656+jshahsjc@users.noreply.github.com>
2021-06-27 16:22:31 -0700

    Update nFactorial.py

--- revision2021/nFactorial.py
+++ revision2021/nFactorial.py
@@ -1,8 +1,8 @@
 def nFactorial(n):
-    while n > 1:
+    if n > 1:
         return n * nFactorial(n - 1)
+    elif n == 1 or n == 0::
+        return 1
     else:
-        if n == 1 or n == 0:
-            return 1
-
+        return -1
 print nFactorial(5)
