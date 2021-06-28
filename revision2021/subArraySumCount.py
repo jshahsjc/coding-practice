84e26b1144b7c2171606111e2e1f7d0b4c0c0605
Jaimin Shah <62130656+jshahsjc@users.noreply.github.com>
2021-06-27 17:25:41 -0700

    Create subArraySumCount.py

---
+++ revision2021/subArraySumCount.py
@@ -0,0 +1,28 @@
+"""
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
+Note:
+The length of the array is in range [1, 20,000].
+The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+"""
+
+def subArraySum(A, sum):
+    if len(A) in range(1, 20001):
+        tmp = i = 0
+        count = 0
+        while tmp != sum and i <= len(A):
+            tmp += A[i]
+            i += 1
+        else:
+            count += 1
+            i += 1
+        return count
+    else:
+        return -1
+
+nums = [1,1,1]
+k = 2
+print subArraySum(nums, k)    
