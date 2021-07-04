d5f262eb2be530eaabcf631b039fb4b7b43e8448
Jaimin Shah <62130656+jshahsjc@users.noreply.github.com>
2021-07-03 17:22:07 -0700

    Create validPallindrome.py

---
+++ revision2021/pass1/validPallindrome.py
@@ -0,0 +1,44 @@
+"""
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+"""
+
+def validPalindrome(S):
+    """
+    S: sentence
+    return bool
+    """
+    temp = ""
+    for s in S:
+        if s.isalnum():
+            temp += s
+
+    i, j = 0, len(temp) - 1
+    while i < j:
+        if temp[i].lower() == temp[j].lower():
+            i += 1
+            j -= 1
+        else:
+            return False
+
+    return True
+
+S = "A man, a plan, a canal: Panama"
+
+if validPalindrome(S):
+    print("yes !!")
