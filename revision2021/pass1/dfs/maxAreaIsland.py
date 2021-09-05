316482f73cf926be0bebddfb708b0962562b431c
Jaimin Shah <62130656+jshahsjc@users.noreply.github.com>
2021-09-04 19:13:18 -0700

    practice

--- revision2021/pass1/dfs/maxAreaIsland.py
+++ revision2021/pass1/dfs/maxAreaIsland.py
@@ -79,3 +79,79 @@ def maxAreaIsland(grid):
             if grid[r][c] == 1:
                 max_area = max(max_area, dfs(r, c))
     return max_area
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Example 1:
+Input: grid = [
+[0,0,1,0,0,0,0,1,0,0,0,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,1,1,0,1,0,0,0,0,0,0,0,0],
+[0,1,0,0,1,1,0,0,1,0,1,0,0],
+[0,1,0,0,1,1,0,0,1,1,1,0,0],
+[0,0,0,0,0,0,0,0,0,0,1,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,0,0,0,0,0,0,1,1,0,0,0,0]
+]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+
+"""
+
+
+def maxAreaIsland(grid):
+    max_area = 0
+    def dfs(r, c):
+        area = 0
+        if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 1:
+            area += 1
+            grid[r][c] = 0
+            if r + 1 < len(grid):
+                area += dfs(r + 1, c)
+            if r - 1 >= 0:
+                area += dfs(r - 1, c)
+            if c + 1 < len(grid[0]):
+                area += dfs(r, c + 1)
+            if c - 1 >= 0:
+                area += dfs(r, c - 1)
+        return area
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            max_area = max(max_area, dfs(r, c))
+    return max_area
--- revision2021/pass1/dicts/fbDinoDict.py
+++ revision2021/pass1/dicts/fbDinoDict.py
@@ -30,6 +30,71 @@ Struthiomimus,1.24,bipedal
 Velociraptorr,2.62,bipedal
 
 """
+from math import sqrt
+from collections import defaultdict
+import csv
+
+def getSpeed(str_len, leg_len):
+    return ((str_len / leg_len) - 1) * sqrt(leg_len * 9.8)
+
+def getBipedalDino(datafile1, datafile2):
+    keep = defaultdict(dict)
+    res = defaultdict()
+    res_list = []
+    with open(datafile2) as f:
+        csv_f = csv.reader(f)
+        # This will create csv_f obj as list of list of strs ( 1 list per line of data )
+        for data in csv_f:
+            if data[2] == 'bipedal':
+                keep[data[0]]['stance'] = 'bipedal'
+                keep[data[0]]['str_len'] = float(data[1])
+    with open(datafile1) as f:
+        csv_f = csv.reader(f)
+        for data in csv_f:
+            if data[0] in keep:
+                keep[data[0]]['leg_len'] = float(data[1])
+    for k, v in keep.items():
+        if 'str_len' and 'leg_len' in v:
+            res[k] = getSpeed(v['str_len'], v['leg_len'])
+    for name, speed in sorted(res.items(), key = lambda x: x[1], reverse = True):
+        print(name)
+    #    res_list.append((name, speed))
+    #return res_list
+
+
+
+"""
+Asked in FB coding screen
+
+You will be supplied with two data files in CSV format .
+The first file contains statistics about various dinosaurs. The second file contains additional data.
+Given the following formula, speed = ((STRIDE_LENGTH / LEG_LENGTH) - 1) * SQRT(LEG_LENGTH * g)
+Where g = 9.8 m/s^2 (gravitational constant)
+
+Write a program to read in the data files from disk, it must then print the names of only the bipedal dinosaurs from fastest to slowest.
+Do not print any other information.
+
+$ cat dataset1.csv
+NAME,LEG_LENGTH,DIET
+Hadrosaurus,1.4,herbivore
+Struthiomimus,0.72,omnivore
+Velociraptor,1.8,carnivore
+Triceratops,0.47,herbivore
+Euoplocephalus,2.6,herbivore
+Stegosaurus,1.50,herbivore
+Tyrannosaurus Rex,6.5,carnivore
+
+$ cat dataset2.csv
+NAME,STRIDE_LENGTH,STANCE
+Euoplocephalus,1.97,quadrupedal
+Stegosaurus,1.70,quadrupedal
+Tyrannosaurus Rex,4.76,bipedal
+Hadrosaurus,1.3,bipedal
+Deinonychus,1.11,bipedal
+Struthiomimus,1.24,bipedal
+Velociraptorr,2.62,bipedal
+
+"""
 from collections import defaultdict
 from math import sqrt
 
